c5aea096f2e8270983ee8123eadb1e071e73b133
Flo <ferrereflorent@hotmail.fr>
2022-10-21 20:39:26 +0400

    updat requirements

--- app.py
+++ app.py
@@ -244,4 +244,5 @@ app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=1800)
 
 
 # jwt = JWT(app, karaoke_list_auth, identity)
-# app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
